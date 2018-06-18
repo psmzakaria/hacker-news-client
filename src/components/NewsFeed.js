a470c7528ab3171fbf713c512f854cec0d59a32f
psmzakaria <psmzakaria@gmail.com>
2018-06-18 16:35:22 +0800

    To do later

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
+import { articles } from "../seedData.js";
 
 class NewsFeed extends Component {
   constructor() {
     super();
     this.state = {
-      articles: []
+      articles: articles
     };
-  }
+
+  //  addVote() {
+  //   return this.state.articles.map((article, i) => {
+
+  //   const newInput =Number( this.state.article.vote)
+  //   this.setState (const newValue = newInput + 1)
+  //   }
+  //  }
+  // setState(
+    
+     
+  
+  // )
+
+
+
+
+
+  } 
 
   render() {
-    return this.state.articles.map((article, i) => {
-      // TODO: start coding!
-      return <li key={i}></li>;
+      return this.state.articles.map((article, i) => {
+        console.log(article.source)
+      return (
+        <div key ={i}>
+          {/* <h1> {article.source.id}</h1> */}
+          <h1> {article.title}</h1>
+          <h2> {article.author}</h2>
+          <p> {article.description}</p>
+          <a href> {article.url}</a>
+          {/* <li> <UpvoteButton/>{article.votes}</li> */}
+
+        </div>
+      );
     });
   }
 }
---
+++ src/components/UpvoteButton.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// const upvoteButton () ={
+
+// <button onClick={() => this.addVote()}>+</button>
+// }
+// //  addVote = () =>{
+
+// const newInput =Number( this.state.article.vote)
+// return (const newValue = newInput + 1)
+//  }
+ 
+//  this.setState({
+//     : [
+     
+//     ]
+//   });
